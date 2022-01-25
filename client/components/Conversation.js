5ed6dc07f491cfc66bcc8c601ecdcbc9dd47bbaa
rjhelm <rjhelm@github.io>
2022-01-24 17:23:04 -0700

    created conversations and a list of conversations

--- client/components/Conversation.js
+++ client/components/Conversation.js
@@ -1,5 +1,5 @@
-import MessageList from './MessageList.js';
-import Avatar from './ui/Avatar.js';
+import MessageList from "./MessageList";
+import Avatar from "./ui/Avatar";
 
 export default class Conversation extends React.Component {
     constructor(props) {
@@ -11,25 +11,29 @@ export default class Conversation extends React.Component {
         this.scrollToBottom();
     }
 
+    componentDidUpdate() {
+        this.scrollToBottom();
+    }
+
     scrollToBottom = () => {
         this.messagesEnd.scrollIntoView({ behavior: "smooth" });
     };
 
-    renderConversationHeader = () => {
+    renderConversationHeader() {
         const { conversation } = this.props;
 
         return (
             <header className="h-16 border-b p-4 flex justify-between items-center">
                 <div className="flex items-center">
-                    <Avatar className="mr-4" initial={conversation.name} size="lg" />
+                    <Avatar className="mr-4" initials={conversation.name} size="large" />
                     <div className="font-bold">{conversation.name}</div>
                 </div>
                 <div className="flex">
                     {conversation.participants.map(participant => (
-                        <Avatar 
+                        <Avatar
                             key={participant.id}
                             initials={participant.username}
-                            style={{ marginRight:  -7 }}
+                            style={{ marginRight: -7 }}
                         />
                     ))}
                 </div>
@@ -43,7 +47,48 @@ export default class Conversation extends React.Component {
         return (
             <div className="flex-1 p-4 overflow-y-scroll scrolling-touch">
                 <MessageList messages={conversation.texts} />
+                <div
+                    ref={el => {
+                        this.messagesEnd = el;
+                    }}
+                />
             </div>
-        )
+        );
+    }
+
+    renderConversationFooter = () => {
+        const { onTextMessageSend } = this.props;
+
+        return (
+            <footer className="h-16 border-t">
+                <form className="h-full" onSubmit={e => e.preventDefault()}>
+                    <input
+                        type="text"
+                        className="block w-full h-full p-4 outline-none"
+                        placeholder="Type something..."
+                        value={this.state.text}
+                        onChange={e => this.setState({ text: e.target.value })}
+                        onKeyPress={e => {
+                            const text = e.target.value;
+                            if (e.key === "Enter" && text !== "") {
+                                e.preventDefault();
+                                onTextMessageSend && onTextMessageSend(text);
+                                this.setState({ text: "" });
+                            }
+                        }}
+                    />
+                </form>
+            </footer>
+        );
+    };
+
+    render() {
+        return (
+            <div className="flex flex-col h-full">
+                {this.renderConversationHeader()}
+                {this.renderConversationBody()}
+                {this.renderConversationFooter()}
+            </div>
+        );
     }
 }
